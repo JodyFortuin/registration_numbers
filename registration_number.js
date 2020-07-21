9d270b50a6a322a1cae96981c92944f5c5176c0a
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-07-21 17:12:25 +0200

    Add files via upload

---
+++ registration_number.js
@@ -0,0 +1,50 @@
+const regFact = regNumFactory(); 
+
+const addBtn = document.querySelector(".addBtn")
+const showBtn = document.querySelector(".showBtn")
+const regPlatesElem = document.querySelector(".regPlates")/*display*/
+const regTextElem = document.querySelector(".regNumText")/*textbox*/
+const errorTextElem = document.querySelector(".error")
+
+function append(){
+    var regList = document.getElementById(regPlatesElem),
+    listNode = document.createTextNode(regNumberEntered),
+    list2 = document.createElement('li');
+
+    list2.appendChild(listNode);
+    regList.appendChild(list2);
+}
+
+function addPlates(list) {
+
+   /*var regNumberEntered = document.getElementById(regTextElem).value,
+        regList = document.getElementById(regPlatesElem),
+
+        list = document.createElement("li"),
+        listNode = document.createTextNode(regNumberEntered);
+        
+        list.appendChild(listNode);
+        regList.appendChild(list); */
+        regPlatesElem.innerHTML = "";
+		for (var i=0 ; i<list.length;i++){
+        const newList = document.createElement("li");
+        const regsListed = list[i];
+        newList.innerHTML = regsListed;
+        regPlatesElem.appendChild(newList);
+        }
+        
+        var regNumberEntered = regTextElem.value;
+        var plateNum = regFact.regNumRegex(regNumberEntered)
+        regPlatesElem.innerHTML = regFact.location(plateNum);
+        if (plateNum !== "") {
+        append()
+        errorTextElem.innerHTML = "";
+        } else {
+            errorTextElem.innerHTML = "please enter regNumber"
+        }
+}
+console.log(regPlatesElem);
+addBtn.addEventListener('click', addPlates);
+
+
+//get display regPlates to show whole list
